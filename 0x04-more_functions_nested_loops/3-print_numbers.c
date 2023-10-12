e350e80d56b4cae0dd632489f002071fbdf8c266
wesam-c <wesameldeeb15@gmail.com>
2023-10-12 14:20:23 -0700

    2 and 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints numbers from 0-9
+ *		followed by a new line
+ *		only use _putchar twice
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9)
+	_putchar('\n');
+}
