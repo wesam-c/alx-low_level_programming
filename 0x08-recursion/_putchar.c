182402b5a3f0c6efc4eeb092315f27edbbaa6004
wesam-c <wesameldeeb15@gmail.com>
2023-10-25 19:06:36 -0700

    assign 0 recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: the character to print
+ *
+ * return: on success 1
+ * on error, -1 is returned
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
