f3c5a312f1a1ca882df9250c29a1fb1b73718917
wesam-c <wesameldeeb15@gmail.com>
2023-11-09 14:53:28 -0800

    add:assign 3 - not complete

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds 2 integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts 2 integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - divides 2 integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the quotient
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mul - multiplies 2 integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the product
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - module 2 integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
