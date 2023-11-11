a2a478bfc6d13b90a95a36706e331cc34ece4c02
wesam-c <wesameldeeb15@gmail.com>
2023-11-10 16:48:42 -0800

    fix: assign 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,20 +6,18 @@
  * @n: The number of paramters passed to the function.
  * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * Return: the integers to sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0, i = n;
 	va_list nums;
-	unsigned int index, sum = 0;
 
+	if (!n)
+		return (0);
 	va_start(nums, n);
-
-	for (index = 0; index < n; index++)
+	while (i--)
 		sum += va_arg(nums, int);
-
 	va_end(nums);
-
 	return (sum);
 }
