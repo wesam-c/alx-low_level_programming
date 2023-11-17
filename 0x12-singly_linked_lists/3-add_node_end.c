e2522bfdfeef900a890734f31fd51613a891d65e
wesam-c <wesameldeeb15@gmail.com>
2023-11-16 18:23:59 -0800

    b

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_head = malloc(sizeof(list_t));
+	list_t *new_node = malloc(sizeof(list_t));
 	list_t *node = *head;
 
 	if (!head || !new_node)
