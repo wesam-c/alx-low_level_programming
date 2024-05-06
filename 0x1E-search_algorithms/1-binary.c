80d1529dd57c17a58aa5d67eb2585ae4b84b85a7
wesam-c <wesameldeeb15@gmail.com>
2024-05-06 10:12:17 +0400

    ADD

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+* binary_search - searches for value in sorted array of
+*                integers using Binary search algorithm
+* @array: pointer to the 1st element in array
+* @size: num of elements in array
+* @value: to search for
+* array is sorted in asc order, value only appear once
+* Return: index where value is located
+*         value not present, array = NULL => -1
+*/
