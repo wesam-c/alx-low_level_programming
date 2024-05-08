21339f4eee69ba48f322a20400daedaf0c87fde6
wesam-c <wesameldeeb15@gmail.com>
2024-05-08 14:02:55 +0400

    ADD: advanced tasks

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,3 +10,41 @@
 * Return: index where value is located
 *         value not present, array = NULL => -1
 */
+
+int binary_search(int *array, size_t size, int value)
+{
+    int low = 0;
+    int high = size - 1;
+    int mid, k;
+
+    if (array == NULL)
+    {
+        return -1;
+    }
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+        for (k = low; k <= high; k++)
+        {
+            if (k == i)
+                printf("Searching in array: %d", array[k]);
+            else
+                printf(", %d", array[k]);
+        }
+        printf("\n");
+
+        if (array[mid] == value)
+        {
+            return mid;
+        }
+        else if (array[mid] < value)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+* jump_search -  searches for value in sorted array
+*               of integers using Jump search algorithm
+* @array: pointer to the 1st element in array
+* @size: num of elements in array
+* @value: to search for
+* array is sorted in asc order
+* Return: index where value is located
+*         value not present, array = NULL => -1
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, jump, step;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
+}
